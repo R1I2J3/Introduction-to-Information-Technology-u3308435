b4cda7ab8f578740277492719d5c278987d14624
Rahman Ishtiaque Joy <u3308435@uni.canberra.edu.au>
2025-08-20 19:19:05 +1000

    Add files via upload

---
+++ Tutorial 4/Tutorial 4.py
@@ -0,0 +1,100 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+= RESTART: F:/UC Studies/ICT and Engineering Research Methodology (12090)/Tutorial Week 3/coffee_shop.py
+Welcome to the Python Coffee Shop!
+
+= RESTART: F:/UC Studies/ICT and Engineering Research Methodology (12090)/Tutorial Week 3/coffee_shop.py
+Welcome to the Python Coffee Shop!
+What is your name? Joy
+Hello, Joy! Let's order some coffee.
+
+= RESTART: F:/UC Studies/ICT and Engineering Research Methodology (12090)/Tutorial Week 3/coffee_shop.py
+Welcome to the Python Coffee Shop!
+What is your name? Joy
+Hello, Joy! Let's order some coffee.
+Coffee: $3.5
+Latte: $4.0
+
+= RESTART: F:/UC Studies/ICT and Engineering Research Methodology (12090)/Tutorial Week 3/coffee_shop.py
+Welcome to the Python Coffee Shop!
+What is your name? Joy
+Hello, Joy! Let's order some coffee.
+Coffee: $4.5
+Latte: $5.0
+What would you like to order? (coffee/latte): 
+= RESTART: F:/UC Studies/ICT and Engineering Research Methodology (12090)/Tutorial Week 3/coffee_shop.py
+Welcome to the Python cappuccino Shop!
+What is your name? Joy
+Hello, Joy! Let's order some cappuccino.
+cappuccino: $4.5
+Latte: $5.0
+What would you like to order? (cappuccino/latte): cappuccino
+How many cups would you like? 3
+You get a discount of $1.00!
+Your total is: $12.5
+Thank you, Joy! Please come again.
+
+= RESTART: F:/UC Studies/ICT and Engineering Research Methodology (12090)/Tutorial Week 3/coffee_shop.py
+Welcome to the Python cappuccino Shop!
+What is your name? Joy
+Hello, Joy! Let's order some cappuccino.
+cappuccino: $4.5
+Latte: $5.0
+mocha: $6.0
+What would you like to order? (cappuccino/latte/mocha): mocha
+How many cups would you like? 4
+You get a discount of $1.00!
+Are you a student? (yes/no) yes
+You get a 10% student discount!
+Your total is: $20.7
+Thank you, Joy! Please come again.
+468 + 751
+1219
+print("How are you?")
+How are you?
+print(8 / 3, 4 * 7, 9 + 13, 2 ** 5, 6 * (3 + 2))
+2.6666666666666665 28 22 32 30
+size = 77
+x = 5
+ y = 7
+ print(abs(x - y) - 10)
+...  print(int(x ** 2) + 1.4)
+...  print(round(y + 3.14159, 2))
+...  
+SyntaxError: multiple statements found while compiling a single statement
+>>> x = 5
+... y = 7
+... print(abs(x - y) - 10)
+... print(int(x ** 2) + 1.4)
+... print(round(y + 3.14159, 2))
+SyntaxError: multiple statements found while compiling a single statement
+>>> x = 5
+>>> =7
+SyntaxError: invalid syntax
+>>> y = 7
+>>> print(abs(x - y) - 10)
+-8
+>>> print(int(x ** 2) + 1.4)
+26.4
+>>> print(round(y + 3.14159, 2))
+10.14
+>>> a = 31
+>>> b = 7
+>>> print(a // b, a % b)
+4 3
+>>> total_minutes = 250
+>>> hours = total_minutes // 60
+>>> minutes = total_minutes % 60
+>>> print("Total time:")
+Total time:
+>>> print("Hours:", hours)
+Hours: 4
+>>> print("Minutes:", minutes)
+Minutes: 10
+>>> str1 = "it is what it is"
+>>> print(str1.find("is"), str1.rfind("it"), str1[-9:-7])
+3 11 ha
+>>> str1 = "it is what it is"
+>>> print(str1[-9:])
+hat it is
